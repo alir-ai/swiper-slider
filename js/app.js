8ac3c3c35f7e1c55871121b2dfa28031dd685993
Alir.Ai <alirezaahmdi11@gmail.com>
2023-12-27 18:13:29 +0330

    final commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  slidesPerView: "auto",
+  spaceBetween: 40,
+  centeredSlides: true,
+  grabCurser: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
